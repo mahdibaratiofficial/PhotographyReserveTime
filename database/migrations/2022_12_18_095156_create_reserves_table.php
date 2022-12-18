c4415741e3e250c4e155d55962c6a512e3306943
Mahdi <mahdibarati696@gmail.com>
2022-12-18 13:30:17 +0330

    add Migration and Model of Reserve

---
+++ database/migrations/2022_12_18_095156_create_reserves_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reserves', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('type')->cascadeOnDelete();
+
+            $table->text('description')->nullable();
+
+            $table->string('number')->nullable();
+
+            $table->enum('norification_type',['sms','ring','email','none']);
+
+            $table->timestamp('reserve_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reserves');
+    }
+}
